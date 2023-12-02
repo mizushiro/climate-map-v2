033c9c57cbc59d934d2bc24f976c22f50d3eccc1
asever77 <johyunmin77@gmail.com>
2023-12-03 01:21:48 +0900

    ㅎㅎ 이쁜사람누규?

--- resources/js/ui.common.js
+++ resources/js/ui.common.js
@@ -131,120 +131,298 @@ $(document).ready(function(){
         'itemSelector' : '.accordion-group'
     });
 });
+(() => {
 
-const UIexe = {};
-UIexe.layerFullSmall = () => {
-    const fulls = document.querySelectorAll('.btn-full-view');
-    const actLayerFullSmall = (e) => {
-        const _this = e.currentTarget;
-        _this.parentNode.parentNode.classList.toggle('active');
-    }
-    for (let item of fulls) {
-        item.addEventListener('click', actLayerFullSmall);
-    }
+	'use strict';
 
-}
-UIexe.inputClear = () => {
-    const inputClear = document.querySelector('.btn-clear');
-    const actClear = (e) => {
-        const el = e.currentTarget;
-        const id = el.dataset.id;
+	const global = 'UIexe';
 
-        document.querySelector('input[data-id="'+ id +'"]').value = '';
-    }
-    inputClear.addEventListener('click', actClear);
-}
-UIexe.hoverLayer = (opt) => {
-    const btnLayerHovers = document.querySelectorAll('.' + opt.classname);
-    const actHover = (e) => {
-        const _this = e.currentTarget;
-        _this.classList.toggle('selected');
-    }
-    for (let item of btnLayerHovers) {
-        item.addEventListener('mouseover', actHover);
-        item.addEventListener('mouseout', actHover);
-    }
-}
-UIexe.loading = {
-    timerShow : {}, 
-    timerHide : {},
-    options : {
-        selector: null,
-        message : 'LOADING',
-        styleClass : 'spinner-area' //time
-    },
-    show(option){
-        const opt = Object.assign({}, this.options, option);
-        const selector = opt.selector; 
-        const styleClass = opt.styleClass; 
-        const message = opt.message;
-        const el = (selector !== null) ? selector : document.querySelector('body');
-        const el_loadingHides = document.querySelectorAll('.ui-loading:not(.visible)');
+	window[global] = {};
 
-        for (let i = 0, len = el_loadingHides.length; i < len; i++) {
-            const that = el_loadingHides[i];
+    const Global = window[global];
+    const UA = navigator.userAgent.toLowerCase();
+    const deviceSize = [1920, 1600, 1440, 1280, 1024, 960, 840, 720, 600, 480, 400, 360];
+    const deviceInfo = ['android', 'iphone', 'ipod', 'ipad', 'blackberry', 'windows ce', 'windows','samsung', 'lg', 'mot', 'sonyericsson', 'nokia', 'opeara mini', 'opera mobi', 'webos', 'iemobile', 'kfapwi', 'rim', 'bb10'];
+    Global.state = {
+        isSystemModal: false,
+        device: {
+            info: (() => {
+                for (let i = 0, len = deviceInfo.length; i < len; i++) {
+                    if (UA.match(deviceInfo[i]) !== null) {
+                        return deviceInfo[i];
+                    }
+                }
+            })(),
+            width: window.innerWidth,
+            height: window.innerHeight,
+            breakpoint: null,
+            colClass: null,
+            ios: (/ip(ad|hone|od)/i).test(UA),
+            android: (/android/i).test(UA),
+            app: UA.indexOf('appname') > -1 ? true : false,
+            touch: null,
+            mobile: null,
+            os: (navigator.appVersion).match(/(mac|win|linux)/i)
+        },
+        browser: {
+            ie: UA.match(/(?:msie ([0-9]+)|rv:([0-9\.]+)\) like gecko)/i),
+            local: (/^http:\/\//).test(location.href),
+            firefox: (/firefox/i).test(UA),
+            webkit: (/applewebkit/i).test(UA),
+            chrome: (/chrome/i).test(UA),
+            opera: (/opera/i).test(UA),
+            safari: (/applewebkit/i).test(UA) && !(/chrome/i).test(UA),	
+            size: null
+        },
+        scroll: {
+            y: 0,
+            direction: 'down'
+        },		
+        breakPoint: [600, 1024],
+        resizeState() {
+            let timerWin;
 
-            that.remove();
+            const act = () => {
+                const browser = Global.state.browser;
+                const device = Global.state.device;
+
+                device.width = window.innerWidth;
+                device.height = window.innerHeight;
+
+                device.touch = device.ios || device.android || (document.ontouchstart !== undefined && document.ontouchstart !== null);
+                device.mobile = device.touch && (device.ios || device.android);
+                device.os = device.os ? device.os[0] : '';
+                device.os = device.os.toLowerCase();
+
+                device.breakpoint = device.width >= deviceSize[5] ? true : false;
+                device.colClass = device.width >= deviceSize[5] ? 'col-12' : device.width > deviceSize[8] ? 'col-8' : 'col-4';
+
+                if (browser.ie) {
+                    browser.ie = browser.ie = parseInt( browser.ie[1] || browser.ie[2] );
+                    ( 11 > browser.ie ) ? support.pointerevents = false : '';
+                    ( 9 > browser.ie ) ? support.svgimage = false : '';
+                } else {
+                    browser.ie = false;
+                }
+                
+                const clsBrowser = browser.chrome ? 'chrome' : browser.firefox ? 'firefox' : browser.opera ? 'opera' : browser.safari ? 'safari' : browser.ie ? 'ie' + browser.ie : 'other';
+                const clsMobileSystem = device.ios ? "ios" : device.android ? "android" : 'etc';
+                const clsMobile = device.mobile ? device.app ? 'ui-a ui-m' : 'ui-m' : 'ui-d';
+                const el_html = document.querySelector('html');
+
+                el_html.classList.remove('col-12', 'col-8', 'col-4');
+                el_html.classList.add(device.colClass);
+                el_html.classList.add(clsBrowser);
+                el_html.classList.add(clsMobileSystem);
+                el_html.classList.add(clsMobile);
+            
+                const w = window.innerWidth;
+
+                clearTimeout(timerWin);
+                timerWin = setTimeout(() => {
+                    el_html.classList.remove('size-tablet');
+                    el_html.classList.remove('size-desktop');
+                    el_html.classList.remove('size-mobile');
+                        el_html.classList.remove('size-desktop');
+
+                    if (w < Global.state.breakPoint[0]) {
+                        Global.state.browser.size = 'mobile';
+                        el_html.classList.add('size-mobile');
+                    } else if (w < Global.state.breakPoint[1]) {
+                        Global.state.browser.size = 'tablet';
+                        el_html.classList.add('size-tablet');
+                    } else {
+                        Global.state.browser.sizee = 'desktop';
+                        el_html.classList.add('size-desktop');
+                    }
+                },200);
+            }
+            window.addEventListener('resize', act);
+            act();
+        },
+    }
+    Global.state.resizeState();
+
+    Global.layerFullSmall = () => {
+        const fulls = document.querySelectorAll('.btn-full-view');
+        const actLayerFullSmall = (e) => {
+            const _this = e.currentTarget;
+            _this.parentNode.parentNode.classList.toggle('active');
+        }
+        for (let item of fulls) {
+            item.addEventListener('click', actLayerFullSmall);
         }
 
-        let htmlLoading = '';
+    }
+    Global.inputClear = () => {
+        const inputClear = document.querySelector('.btn-clear');
+        const actClear = (e) => {
+            const el = e.currentTarget;
+            const id = el.dataset.id;
 
+            document.querySelector('input[data-id="'+ id +'"]').value = '';
+        }
+        inputClear.addEventListener('click', actClear);
+    }
 
-        (selector === null) ?
-            htmlLoading += '<div class="ui-loading spinner-area">':
-            htmlLoading += '<div class="spinner-area type-area">';
+    Global.hoverLayer = (opt) => {
+        const btnLayerHovers = document.querySelectorAll('.' + opt.classname);
+        
+        
+        const actHover = (e) => {
+            const _this = e.currentTarget;
+           
+            _this.classList.add('selected');
+        }
+         const actOut = (e) => {
+            const _this = e.currentTarget;
+           
+            _this.classList.remove('selected');
+        }
+        const actClick = (e) => {
+            
+            const _this = e.currentTarget;
+            const name = _this.dataset.name;
+            const wrap = _this.closest('.' + name);
 
-        htmlLoading += '<div class="spinner-box">';
-        htmlLoading += '<div class="spinner-img"></div><p class="loading-text">'+ message +'</p>';
-        htmlLoading += '</div>';
-        htmlLoading += '</div>';
+            wrap.classList.toggle('selected');
 
-        const showLoading = () => {
-            const el_child = el.childNodes;
-            let is_loading = false;
+        }
+        const actClose = (e) => {
+            const _this = e.currentTarget;
+            const name = _this.dataset.name;
+            const wrap = _this.closest('.' + name);
 
-            for (let i = 0; i < el_child.length; i++) {
-                if (el_child[i].nodeName === 'DIV' && el_child[i].classList.contains('ui-loading')) {
-                    is_loading = true;
-                }
+            wrap.classList.remove('selected');
+        }
+
+        let closeBtn;
+        let btn;
+        for (let item of btnLayerHovers) {
+            closeBtn = item.querySelector('.layer-close');
+            btn = item.querySelector('button[data-hoverlayer="btn"]');
+            closeBtn ? closeBtn.dataset.name = opt.classname : '';
+            btn ? btn.dataset.name = opt.classname : '';
+            closeBtn && closeBtn.addEventListener('click', actClose);
+
+            if (Global.state.device.mobile) {
+                btn.addEventListener('click', actClick);
+            } else {
+                item.addEventListener('mouseout', actOut);
+                item.addEventListener('mouseover', actHover);
+            }
+        }
+    }
+    Global.loading = {
+        timerShow : {}, 
+        timerHide : {},
+        options : {
+            selector: null,
+            message : 'LOADING',
+            styleClass : 'spinner-area' //time
+        },
+        show(option){
+            const opt = Object.assign({}, this.options, option);
+            const selector = opt.selector; 
+            const styleClass = opt.styleClass; 
+            const message = opt.message;
+            const el = (selector !== null) ? selector : document.querySelector('body');
+            const el_loadingHides = document.querySelectorAll('.ui-loading:not(.visible)');
+
+            for (let i = 0, len = el_loadingHides.length; i < len; i++) {
+                const that = el_loadingHides[i];
+
+                that.remove();
             }
 
-            !is_loading && el.insertAdjacentHTML('beforeend', htmlLoading);
-            htmlLoading = null;		
-            
-            const el_loadings = document.querySelectorAll('.ui-loading');
+            let htmlLoading = '';
 
-            this.timerShow = setTimeout(() => {
+
+            (selector === null) ?
+                htmlLoading += '<div class="ui-loading spinner-area">':
+                htmlLoading += '<div class="spinner-area type-area">';
+
+            htmlLoading += '<div class="spinner-box">';
+            htmlLoading += '<div class="spinner-img"></div><p class="loading-text">'+ message +'</p>';
+            htmlLoading += '</div>';
+            htmlLoading += '</div>';
+
+            const showLoading = () => {
+                const el_child = el.childNodes;
+                let is_loading = false;
+
+                for (let i = 0; i < el_child.length; i++) {
+                    if (el_child[i].nodeName === 'DIV' && el_child[i].classList.contains('ui-loading')) {
+                        is_loading = true;
+                    }
+                }
+
+                !is_loading && el.insertAdjacentHTML('beforeend', htmlLoading);
+                htmlLoading = null;		
+                
+                const el_loadings = document.querySelectorAll('.ui-loading');
+
+                this.timerShow = setTimeout(() => {
+                    for (let i = 0, len = el_loadings.length; i < len; i++) {
+                        const that = el_loadings[i];
+
+                        that.classList.add('visible');
+                        that.classList.remove('close');
+                    }
+                },0);
+                
+            }
+            clearTimeout(this.timerShow);
+            clearTimeout(this.timerHide);
+            showLoading();
+        },
+        hide(){
+            clearTimeout(this.timerShow);
+            this.timerHide = setTimeout(() => {
+                const el_loadings = document.querySelectorAll('.ui-loading');
+
                 for (let i = 0, len = el_loadings.length; i < len; i++) {
                     const that = el_loadings[i];
 
-                    that.classList.add('visible');
-                    that.classList.remove('close');
+                    that.classList.add('close');
+                    setTimeout(() => {
+                        that.classList.remove('visible')
+                        that.remove();
+                    },200);
                 }
-            },0);
-            
+            },200);
         }
-        clearTimeout(this.timerShow);
-        clearTimeout(this.timerHide);
-        showLoading();
-    },
-    hide(){
-        clearTimeout(this.timerShow);
-        this.timerHide = setTimeout(() => {
-            const el_loadings = document.querySelectorAll('.ui-loading');
-
-            for (let i = 0, len = el_loadings.length; i < len; i++) {
-                const that = el_loadings[i];
-
-                that.classList.add('close');
-                setTimeout(() => {
-                    that.classList.remove('visible')
-                    that.remove();
-                },200);
-            }
-        },200);
     }
-}
+    Global.toggle = {
+        init() {
+            const objs = document.querySelectorAll('[data-toggle-obj');
+
+            for (let item of objs) {
+                item.addEventListener('click', Global.toggle.toggle)
+            }
+        },
+        toggle(e) {
+            const _this = e.currentTarget;
+            console.log(_this);
+            const target = document.querySelector('[data-toggle-target="'+ _this.dataset.toggleObj +'"]');
+            if (target) {
+                target.dataset.toggle !== 'true' ? target.dataset.toggle = true: target.dataset.toggle = false;
+            }
+        },
+        show(v) {
+            const target = document.querySelector('[data-toggle-target="'+ v +'"]');
+            if (target) {
+                target.dataset.toggle = true;
+            }
+        },
+        hide(v) {
+            const target = document.querySelector('[data-toggle-target="'+ v +'"]');
+            if (target) {
+                target.dataset.toggle = false;
+            }
+        }
+    }
+})();
 
 class UI_ToggleAct {
     constructor(opt) {
@@ -297,7 +475,7 @@ class UI_ToggleAct {
             }
         } else {
             this.target.dataset.toggle = 'true';
-           
+        
         }
 
         if (!!this.btn) {
@@ -363,7 +541,7 @@ class UI_DragMap {
             per < 0 ? per = 0 : per;
             per > 100 ? per = 100 : per;
             _per = 100 - per;
-           
+        
             divide_line.style.left = per + '%';
 
             if (this.divide_items.length === 2) {
@@ -411,3 +589,23 @@ class UI_DragMap {
         window.addEventListener('mouseup', actEnd);
     }
 }
+document.addEventListener('DOMContentLoaded', function () {
+    //layerpopup
+    newhome.callLayer();
+    //nav
+    // UIexe.navToggle = new UI_ToggleAct({
+    //     target: document.querySelector('.head'),
+    //     btn: document.querySelectorAll('.head-nav-btn'),
+    //     callback_show: () => {
+    //         console.log('navToggle show');
+    //     },
+    //     callback_hide: () => {
+    //         console.log('navToggle hide'); 
+    //     },
+    // });
+    //hover layer
+    UIexe.toggle.init();
+    UIexe.hoverLayer({
+        classname : 'btn-layer-hover'
+    });
+}, false);
